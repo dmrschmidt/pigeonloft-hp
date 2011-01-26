2fc92e6250ebabdb0d03cc6ffea4571acbebff83
Dennis Schmidt <metzeltiger@web.de>
2011-01-26 19:55:11 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigeonloftHp::Application.config.secret_token = '5f7210ad74dd112165628c6a300268c9e27e481c5ab2d4f3d97b4c8ce35fc18471d5aec8e292bdc24a7b3f8f66490c4747269a9d1921a02406c58c4c53969131'
