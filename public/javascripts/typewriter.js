747cf3bc1ec6860e414bb08682c22c1a54524573
Dennis Schmidt <metzeltiger@web.de>
2011-02-16 17:31:11 +1100

    improved typewriter script

--- public/javascripts/typewriter.js
+++ public/javascripts/typewriter.js
@@ -1,30 +1,206 @@
-// var MAX_ITERATIONS = 5;
-// var row1 = "40.754983,-73.984027";
-// var row2 = "next: San Francisco, CA";
-// var fixed = "";
-// var size = 10;
-// var position = 0;
-// var iteration = 0;
-// 
-// function getChar() {
-//   return (iteration < MAX_ITERATIONS - 1)
-//     ? Math.floor(Math.random() * 10)
-//     : row1[position];
-// }
-// 
-// function type() {
-//   var current = fixed + getChar();
-//   $("#current var").replaceWith('<var>' + current + '</var>');
-//   iteration = (iteration < MAX_ITERATIONS) ? (iteration + 1) : 0 ;
-//   if(iteration == 0) {
-//     fixed = current;
-//     position++;
-//   }
-//   window.setTimeout("type()", 100);
-// }
-// 
-// $(document.body).click(function () {
-//   if ($("#speech_bubble").is(":hidden")) {
-//     $("#speech_bubble").show("fast", type());
-//   }
-// });
+/**
+ *
+ */
+var Typebox = $.Class.create({
+    /*
+     * properties
+     */
+    _element : null,
+    _text : "",
+    _fixed : "",
+    _current : "",
+    _position : 0,
+    _iteration : 0,
+    _max_iterations: 3,
+    _waited : 0,
+    _started : false,
+    
+    /*
+     *
+     */
+    initialize: function(element) {
+      this._element = $(element);
+      this._text = this._element.attr("data-text");
+    },
+    
+    /*
+      *
+      */
+     get_inner: function() {
+       return $(this._element.find("var").first());
+     },
+    
+    /*
+     * Calculates and returns the next character to be displayed.
+     */
+    get_char: function() {
+      if(this.is_waiting()) {
+        var value = '.';
+        this._fixed = this._current;
+        if(++this._waited == 10) {
+          this._fixed = "";
+          value = '';
+        }
+      } else {
+        var value = (this._iteration < (this._max_iterations - 1))
+          ? Math.floor(Math.random() * 10)
+          : this._text[this._position];
+        this._iteration = (this._iteration + 1) % (this._max_iterations + 1);
+        if(this._iteration == 0) {
+          this._position++;
+          this._fixed = this._current;
+        }
+      }
+      return value;
+    },
+    
+    /*
+     *
+     */
+    is_done: function() {
+      return this._position >= this._text.length;
+    },
+    
+    /*
+     *
+     */
+    is_waiting: function() {
+      return this._element.attr("data-prefill") == "true" && this._waited < 10;
+    },
+    
+    /*
+     *
+     */
+    update: function() {
+      if(!this.is_done()) {
+        this._current = this._fixed + this.get_char();
+        this.get_inner().replaceWith('<var>' + this._current + '</var>');
+      }
+      this._started = true;
+      return this.is_done();
+    },
+    
+    /*
+     *
+     */
+    is_firstrun: function() {
+      return !this._started;
+    },
+    
+    /*
+     *
+     */
+    should_pause: function() {
+      return this._element.attr("data-prepause") && this.is_firstrun();
+    },
+    
+    /*
+     *
+     */
+    get_interval: function() {
+      if(this.should_pause()) {
+        return parseInt(this._element.attr("data-prepause"));
+      } else if(this.is_waiting()) {
+        return 500;
+      } else {
+        return 10;
+      }
+    },
+    
+    /*
+     *
+     */
+    toString: function() {
+      return this._element.id;
+    },
+    
+  }, {
+    /*
+     * properties
+     */
+    getset: [['BoxId', '_box_id']]
+});
+
+
+/**
+ *
+ */
+var Typewriter = $.Class.create({
+    /*
+     * properties
+     */
+    _box : null,
+    _max_iterations : null,
+    _fixed : "",
+    _position : 0,
+    _iteration : 0,
+    _waited : 0,
+    _current_text : "messaging decelerated",
+    _parts : [],
+    
+    /*
+     * Constructs a new typewriter for the given DOM Object ID.
+     */
+    initialize: function(box_id) {
+      this._box_id = box_id;
+      this._box = $(box_id);
+      this._max_iterations = parseInt(this._box.attr("data-iterations"));
+      this.load_parts();
+      $(document.body).click(jQuery.proxy(this.register, this));
+    },
+    
+    /*
+     *
+     */
+    load_part: function(part) {
+      this._parts.push(new Typebox(part));
+    },
+    
+    /*
+     *
+     */
+    load_parts: function() {
+      var boxes = $(this._box_id + " .fill_text");
+      var parent = this;
+      $.each(boxes, function(index, element) { parent.load_part(element) });
+    },
+    
+    /*
+     *
+     */
+    register: function() {
+      if(this._box.is(":hidden")) {
+        this._box.show("fast", jQuery.proxy(this.type, this));
+      }
+    },
+    
+    /*
+     *
+     */
+    get_part: function() {
+      if(this._current_part == null || (this._current_part.is_done() && this._parts.length > 0))
+        this._current_part = this._parts.shift();
+      return this._current_part;
+    },
+    
+    /*
+     *
+     */
+    type: function() {
+      this.get_part().update();
+      var timeout = this.get_part().get_interval();
+      window.setTimeout(jQuery.proxy(this.type, this), timeout);
+    },
+    
+    /*
+     * methods
+     */
+    toString: function() {
+      return this.property('BoxId');
+    }
+  }, {
+    /*
+     * properties
+     */
+    getset: [['BoxId', '_box_id']]
+});
