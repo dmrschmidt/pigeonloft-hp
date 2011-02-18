2aa93a54584e03e551fb6b5d3e61a5da0b5fb26d
Dennis Schmidt <metzeltiger@web.de>
2011-02-18 16:51:30 +1100

    - switched to recent jQuery typewriter version - deleted unneeded files - refactoring

--- public/javascripts/typewriter.js
+++ public/javascripts/typewriter.js
@@ -1,3 +1,24 @@
+/*
+ * jQuery typewriter by Dennis Schmidt
+ * free download at: http://metzeltiger.github.com/jquery-typewriter/
+ */
+var DEFAULTS = new Array();
+DEFAULTS['data-timeout-letter'] =  15;
+DEFAULTS['data-timeout-wait']   = 100;
+DEFAULTS['data-iterations']     =   5;
+
+/*
+ * Returns the desired timeout for the given element (DOM object) and type
+ * (timeout-letter or timeout-wait). These have default values that will be
+ * returned if the given element doesn't have a valid data-timeout-XXX
+ * attribute set.
+ */
+function get_value(element, type, default_value) {
+  var timeout = parseInt(element.attr(type));
+  if(isNaN(timeout) || timeout <= 0) { timeout = default_value }
+  return timeout;
+}
+
 /**
  * Wraps an element that is to be filled with text.
  */
@@ -6,11 +27,13 @@ var Typebox = $.Class.create({
      * properties
      */
     _max_waiting : 15,
-    _max_iterations: 3,
+    _possible_chars : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    
     /*
      * Wrap the passed element into a new Typebox.
      */
-    initialize: function(element) {
+    initialize: function(element, should_cycle, timeout_letter, timeout_wait,
+          max_iterations) {
       this._fixed = "";
       this._current = "";
       this._position = 0;
@@ -18,42 +41,117 @@ var Typebox = $.Class.create({
       this._waited = 0;
       this._started = false;
       this._element = $(element);
-      this._text = this._element.attr("data-text");
+      this._text = this._element.html();
+      this._in_tag = false;
+      this._element.html('');
+      this._should_cycle = should_cycle;
+      this._max_iterations = max_iterations;
+      this.set_timeouts(timeout_letter, timeout_wait);
     },
     
     /*
-     * Return the inner <var> tag, that will be replaced by the new text.
+     * Sets the timeouts to use.
      */
-    get_inner: function() {
-      return $(this._element.find("var").first());
+    set_timeouts: function(timeout_letter, timeout_wait) {
+      this._timeout_letter = get_value(this._element,
+          'data-timeout-letter', timeout_letter);
+      this._timeout_wait = get_value(this._element,
+          'data-timeout-wait', timeout_wait);
+    },
+    
+    /*
+     * Returns the current (real) character that is about to be written.
+     */
+    current_char: function() {
+      return this._text[this._position];
+    },
+    
+    /*
+     * Returns a randomly generated character from the list of all possible
+     * characters.
+     */
+    random_char: function() {
+      var index = Math.floor(Math.random() * this._possible_chars.length);
+      return this._possible_chars.charAt(index);
+    },
+    
+    /*
+     * Returns true if the current character should be printed with other
+     * random characters, before actually printing the correct char itself.
+     * This is never the case for special characters, such as whitespaces.
+     */
+    should_cycle: function() {
+      var should_cycle = this._should_cycle &&
+        (this._iteration < (this._max_iterations - 1)) &&
+          !(this.current_char() == ' ' ||
+            this.current_char() == "\n" ||
+            this.current_char() == "\r");
+      return should_cycle;
+    },
+    
+    /*
+     * What to do when we are waiting.
+     */
+    handle_waiting: function() {
+      var value = '.';
+      this._fixed = this._current;
+      // if waiting is finished, reset the text field
+      if(++this._waited == this._max_waiting) {
+        this._fixed = "";
+        value = '';
+      }
+      return value;
+    },
+    
+    /*
+     * What to do when we are writing actual text.
+     */
+    handle_writing: function() {
+      var value = this.should_cycle()
+        ? this.random_char()
+        : this.current_char();
+      this._iteration = (this._iteration + 1) % (this._max_iterations + 1);
+      if(this._iteration == 0) {
+        this._position++;
+        this._fixed = this._current;
+      }
+      return value;
     },
     
     /*
      * Calculates and returns the next character to be displayed.
      */
     get_char: function() {
-      // display dots if in "waiting" mode
       if(this.is_waiting()) {
-        var value = '.';
-        this._fixed = this._current;
-        // if waiting is finished, reset the text field
-        if(++this._waited == this._max_waiting) {
-          this._fixed = "";
-          value = '';
-        }
-      // display random letter or real character when not waiting
+        // display dots if in "waiting" mode
+        var value = this.handle_waiting();
       } else {
-        var value = (this._iteration < (this._max_iterations - 1))
-          ? Math.floor(Math.random() * 10)
-          : this._text[this._position];
-        this._iteration = (this._iteration + 1) % (this._max_iterations + 1);
-        if(this._iteration == 0) {
-          this._position++;
-          this._fixed = this._current;
-        }
+        // display random letter or real character when not waiting
+        var value = this.handle_writing();
       }
       return value;
     },
+    
+    /*
+     * Returns true, when currently an HTML tag needs to be written.
+     * Needed to quickly jump over these, to avoid ugliness.
+     */
+    in_tag: function() {
+      var current = this.current_char();
+      this._in_tag = (this._in_tag || current == '<') && current != '>';
+      return this._in_tag;
+    },
+    
+    /*
+     * Quickly writes a tag.
+     */
+    write_tag: function() {
+      this._current = this._fixed + this.current_char();
+      $(this._element).html(this._current);
+      this._fixed = this._current;
+      this._position++;
+    },
+    
     
     /*
      * Returns true if this Typebox is finished printing it's text.
@@ -66,7 +164,8 @@ var Typebox = $.Class.create({
      * Returns true if this Typebox is currently in waiting mode.
      */
     is_waiting: function() {
-      return this._element.attr("data-prefill") == "true" && this._waited < this._max_waiting;
+      return this._element.attr("data-prefill") == "true" &&
+        this._waited < this._max_waiting;
     },
     
     /*
@@ -74,8 +173,13 @@ var Typebox = $.Class.create({
      */
     update: function() {
       if(!this.is_done()) {
-        this._current = this._fixed + this.get_char();
-        this.get_inner().replaceWith('<var>' + this._current + '</var>');
+        // skip any tags quickly
+        if(this.in_tag()) do { this.write_tag(); } while(this.in_tag())
+        // in case we skipped a tag, we need to verify we're not done again
+        if(!this.is_done()) {
+          this._current = this._fixed + this.get_char();
+          $(this._element).html(this._current);
+        }
       }
       this._started = true;
       return this.is_done();
@@ -102,9 +206,9 @@ var Typebox = $.Class.create({
       if(this.should_pause()) {
         return parseInt(this._element.attr("data-prepause"));
       } else if(this.is_waiting()) {
-        return 100;
+        return this._timeout_wait;
       } else {
-        return 10;
+        return this._timeout_letter;
       }
     },
     
@@ -112,8 +216,8 @@ var Typebox = $.Class.create({
      *
      */
     toString: function() {
-      return this._element.attr("data-text");
-    }
+      return this._text;
+    },
 });
 
 
@@ -123,19 +227,42 @@ var Typebox = $.Class.create({
  */
 var Typewriter = $.Class.create({
     /*
-     * properties
+     * Common initializer functions.
      */
+    init: function() {
+      this._parts = [];
+      this._should_cycle = this._box.attr("data-cycling") != "false";
+      this.load_parts();
+      this.autostart();
+    },
+    
+     /*
+      * Constructs a new typewriter for the given DOM Object ID.
+      */
+     initialize: function(element) {
+       this._box_id = element.id;
+       this._box = $(element);
+       this.init();
+     },
     
     /*
      * Constructs a new typewriter for the given DOM Object ID.
      */
     initialize: function(box_id, activator) {
-      this._parts = [];
       this._box_id = box_id;
       this._box = $(box_id);
-      this._max_iterations = parseInt(this._box.attr("data-iterations"));
-      this.load_parts();
+      this.init();
       $('#'+activator).click(jQuery.proxy(this.register, this));
+    },
+    
+    /*
+     * Performs an automatic start unless configured otherwise.
+     * By default this is enabled, but it can be disabled by adding
+     * data-autostart="false" to the main typewriter DOM object
+     * (the one with class 'typewriter').
+     */
+    autostart: function() {
+      if(this._box.attr("data-autostart") != "false") { this.type(); }
     },
     
     /*
@@ -151,14 +278,26 @@ var Typewriter = $.Class.create({
      * Creates a Typebox instance from the given DOM object.
      */
     load_part: function(part) {
-      this._parts.push(new Typebox(part));
+      var timeout_letter = get_value(this._box, 'data-timeout-letter',
+          DEFAULTS['data-timeout-letter']);
+      var timeout_wait = get_value(this._box, 'data-timeout-wait',
+          DEFAULTS['data-timeout-wait']);
+      var max_iterations = get_value(this._box, 'data-iterations',
+          DEFAULTS['data-iterations']);
+      this._parts.push(new Typebox(
+        part,
+        this._should_cycle,
+        timeout_letter,
+        timeout_wait,
+        max_iterations)
+      );
     },
     
     /*
      * Loads all the Typebox instances that are available.
      */
     load_parts: function() {
-      var boxes = this._box.find(".fill_text");
+      var boxes = this._box.find(".typewrite");
       var parent = this;
       $.each(boxes, function(index, element) { parent.load_part(element) });
     },
@@ -167,7 +306,8 @@ var Typewriter = $.Class.create({
      * Returns the Typebox instance that is currently in update progress.
      */
     get_part: function() {
-      if(this._current_part == null || (this._current_part.is_done() && this._parts.length > 0))
+      if(this._current_part == null || (this._current_part.is_done() && 
+          this._parts.length > 0))
         this._current_part = this._parts.shift();
       return this._current_part;
     },
@@ -189,3 +329,10 @@ var Typewriter = $.Class.create({
       return this._box_id;
     }
 });
+
+// $(document).ready(function() {
+//   var typewriters = [];
+//   $(".typewriter").each(function(index, element) {
+//     typewriters.push(new Typewriter(element));
+//   });
+// });
