994c61eb07367f131d4236406f8d5a9862e73c03
Dennis Schmidt <dmrschmidt@gmail.com>
2011-12-14 16:41:12 +0100

    updated site
    - moved launch rock page to /launch
    - added draft app presentation site

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,25 @@
 class StaticController < ApplicationController
+  
+  #
+  # The postcard view page.
+  #
   def postcard
     @postcard_id = params[:view]
     render :postcard, :layout => false
   end
+  
+  #
+  # The launchrock display page.
+  #
+  def launch
+    render :launch, :layout => false
+  end
+  
+  #
+  #
+  #
+  def welcome
+    render :welcome, :layout => "static"
+  end
+  
 end
